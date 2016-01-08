93822d20979ddee5dad33a9da7d63773048f2b6b
Marios Phinikettos <FinikM@gmail.com>
2016-01-08 15:56:39 +0200

    Added tests

--- src/test/java/gr/ntua/image/mediachecker/MediaCheckerTest.java
+++ src/test/java/gr/ntua/image/mediachecker/MediaCheckerTest.java
@@ -4,62 +4,91 @@ import org.junit.Assert;
 import org.junit.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.InterruptedException;
+import org.im4java.core.InfoException;
+import org.im4java.core.IM4JavaException;
 
 public class MediaCheckerTest {
 
 	@Test
-	public void testFileSize() {
-		String img       = "src/test/resources/GED_00000000.JPG";
+	public void testFileSize() throws FileNotFoundException, IOException {
 		String imageFile = "src/test/resources/test.jpg";
+
+		Assert.assertEquals(85489, MediaChecker.getFileSize(imageFile));
+	}
+
+	@Test
+	public void testImage() throws IOException, InfoException, InterruptedException, IM4JavaException {
+		String imageFile = "src/test/resources/test.jpg";
+		String colormap  = "src/test/resources/colormap.png";
+		String[] colors  = new String[] { "#000000", "#DC143C", "#800000", "#B22222", "#A52A2A", "#FF1493" };
+
+		ImageInfo ii = MediaChecker.getImageInfo(imageFile, colormap);
+		Assert.assertEquals("image/jpeg", ii.getMimeType());
+		Assert.assertEquals("JPEG", ii.getFileFormat());
+		Assert.assertEquals(1280, ii.getWidth());
+		Assert.assertEquals(1024, ii.getHeight());
+		Assert.assertEquals("sRGB", ii.getColorSpace());
+		Assert.assertArrayEquals(colors, ii.getPalette());
+	}
+
+	@Test
+	public void testVideo() throws IOException {
+		String videoFile = "src/test/resources/sample_mpeg4.mp4";
+
+		VideoInfo vi = MediaChecker.getVideoInfo(videoFile);
+		Assert.assertEquals(190, vi.getWidth());
+		Assert.assertEquals(240, vi.getHeight());
+		Assert.assertEquals(4966, vi.getDuration());
+		Assert.assertEquals("mpeg4", vi.getCodec());
+		Assert.assertEquals(30, vi.getFrameRate(), 0);
+		Assert.assertEquals("video/mp4", vi.getMimeType());
+		Assert.assertEquals(341948, vi.getBitRate());
+	}
+
+	@Test
+	public void testAudio() throws IOException {
 		String mp3File   = "src/test/resources/tone.mp3";
 		String flacFile  = "src/test/resources/tone16bit.flac";
 		String wavFile   = "src/test/resources/sample26.wav";
-		String videoFile = "src/test/resources/sample_mpeg4.mp4";
-		String pdfFile   = "src/test/resources/scansmpl.pdf";
-		String pdfFile2  = "src/test/resources/pdf.pdf";
-		String pdfFile3  = "src/test/resources/preface.pdf";
-		String pdfFile4  = "src/test/resources/org.pdf";
+
+		AudioInfo ai = MediaChecker.getAudioInfo(mp3File);
+		Assert.assertEquals(5147, ai.getDuration());
+		Assert.assertEquals(16, ai.getBitDepth());
+		Assert.assertEquals(2, ai.getChannels());
+		Assert.assertEquals("audio/mpeg", ai.getMimeType());
+		Assert.assertEquals(320000, ai.getBitRate());
+		Assert.assertEquals(44100, ai.getSampleRate());
+		Assert.assertEquals("mp3", ai.getFileFormat());
+
+		ai = MediaChecker.getAudioInfo(flacFile);
+		Assert.assertEquals("audio/x-flac", ai.getMimeType());
+		Assert.assertEquals("flac", ai.getFileFormat());
+		Assert.assertEquals(0, ai.getBitRate());
+
+		ai = MediaChecker.getAudioInfo(wavFile);
+		Assert.assertEquals("audio/x-wav", ai.getMimeType());
+		Assert.assertEquals("first_audio", ai.getFileFormat());
+		Assert.assertEquals(512000, ai.getBitRate());
+	}
+
+	@Test
+	public void testText() throws IOException, FileNotFoundException {
+		String pdfFile   = "src/test/resources/scansmpl.pdf";	// Single Image + Not searchable
+		String pdfFile2  = "src/test/resources/pdf.pdf";		// Multiple Images + Searchable
+		String pdfFile3  = "src/test/resources/preface.pdf";	// No Images (obviously searchable)
+		String pdfFile4  = "src/test/resources/org.pdf";		// Single Image + Searchable
 		String textFile  = "src/test/resources/sample.txt";
 
-		String colormap  = "src/test/resources/colormap.png";
+		Assert.assertEquals(200, MediaChecker.getDPI(pdfFile));
+		Assert.assertEquals(-1, MediaChecker.getDPI(pdfFile2));
+		Assert.assertEquals(0, MediaChecker.getDPI(pdfFile3));
+		Assert.assertEquals(72, MediaChecker.getDPI(pdfFile4));
 
-		try {
-			Assert.assertTrue(true);
-//			Assert.assertEquals(MediaChecker.getFileSize(imageFile), 146950);
-
-			// ColorPalette.saveColorMap("src/test/resources/colormap.png");
-
-			// MediaChecker.getImageInfo(imageFile, colormap);
-			// System.out.println(MediaChecker.getImageInfo(imageFile, colormap).toString());
-			System.out.println(MediaChecker.getImageInfo(img, colormap).toString());
-
-			// System.out.println(MediaChecker.getAudioInfo(wavFile).toString());
-			// MediaChecker.getVideoInfo(videoFile);
-
-			// System.out.println(MediaChecker.getMimeType(textFile));
-			// System.out.println(MediaChecker.getMimeType(pdfFile));
-			// MediaChecker.isSearchable(pdfFile3);
-			// System.out.println("Sample 1: " + MediaChecker.getDPI(pdfFile));
-			// System.out.println("Sample 2: " + MediaChecker.getDPI(pdfFile2));
-			// System.out.println("Sample 3: " + MediaChecker.getDPI(pdfFile3));
-			// System.out.println("Sample 4: " + MediaChecker.getDPI(pdfFile4));
-//			System.out.println("Full Text 1: " + MediaChecker.isSearchable(pdfFile));
-//			System.out.println("Full Text 2: " + MediaChecker.isSearchable(pdfFile2));
-//			System.out.println("Full Text 3: " + MediaChecker.isSearchable(pdfFile3));
-			// System.out.println("Full Text 4: " + MediaChecker.isSearchable(pdfFile4));
-		}
-		catch (FileNotFoundException fe) {
-			System.out.println("File not found!");
-			// Assert.fail();
-		}
-		catch (IOException ioe) {
-			System.out.println("Error opening the file!");
-			// Assert.fail();
-		}
-		catch (Exception e) {
-			// System.out.println("Uknown Exception!");
-			System.out.println(e.getMessage());
-			Assert.fail();
-		}
+		Assert.assertFalse(MediaChecker.isSearchable(pdfFile));
+		Assert.assertTrue(MediaChecker.isSearchable(pdfFile2));
+		Assert.assertTrue(MediaChecker.isSearchable(pdfFile3));
+		Assert.assertFalse(MediaChecker.isSearchable(pdfFile4));
+		Assert.assertTrue(MediaChecker.isSearchable(textFile));
 	}
 }
